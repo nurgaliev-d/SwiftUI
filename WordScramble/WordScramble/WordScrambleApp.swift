8d5438e4d65ceb91de3e0181fa704e73c6fd2587
Nurgaliev Dias <123064954+nurgaliev-d@users.noreply.github.com>
2025-07-08 18:21:03 +0500

    Word Scramle Game

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Диас Нургалиев on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
