461790f4f5d9b84dc7696a4eda1c8d5eff7b0b2f
Nurgaliev Dias <123064954+nurgaliev-d@users.noreply.github.com>
2025-07-08 15:27:56 +0500

    BetterRestApp

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Диас Нургалиев on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
